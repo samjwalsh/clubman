fb1593a8c561afaea4f3e54363eee18e440e4594
samjwalsh <samjwalsh05@gmail.com>
2025-11-24 11:58:19 +0000

    template

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # clubman
+
+# Stack
+
+This project makes use of:
+
+- [Next.js](https://nextjs.org)
+- [NextAuth.js](https://next-auth.js.org)
+- [Prisma](https://prisma.io)
+- [Drizzle](https://orm.drizzle.team)
+- [Shadcn](https://ui.shadcn.com)
+- [Tailwind CSS](https://tailwindcss.com)
+- [tRPC](https://trpc.io)
---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { headers } from "next/headers";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/server/better-auth";
+import { getSession } from "@/server/better-auth/server";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  const session = await getSession();
+  return (
+    <HydrateClient>
+      <main className="">
+        <div>hi</div>
+      </main>
+    </HydrateClient>
+  );
+}
